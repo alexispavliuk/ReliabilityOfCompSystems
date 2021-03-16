c81b15aa08d5833afdf520c19f0d5bcb90592a35
OleksiiPavliuk <alexispavliuk@gmail.com>
2021-03-16 02:15:58 +0200

    initial commit

---
+++ ReliabilityOfCompSystems/Lab1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ReliabilityOfCompSystems
+{
+    public class Lab1
+    {
+        public Lab1(int k, int[] data)
+        {
+            K = k;
+            Data = data;
+            Data = Data.OrderBy(x => x).ToArray();
+
+            var maxValue = data.Max();
+            IntervalWidth = (double)maxValue / k;
+
+            Intervals = new double[K+1];
+
+            for (int i = 0; i < Intervals.Length; i++)
+                Intervals[i] = i * IntervalWidth;
+        }
+
+        public int K { get; }
+
+        public int[] Data { get; }
+
+        public double IntervalWidth { get; }
+
+        public int N { get => Data.Count(); }
+
+        public double[] Intervals { get; }
+
+
+        private List<int>[] DistributeDataByIntervals()
+        {
+            List<int>[] result = new List<int>[K];
+
+            foreach (var number in Data)
+            {
+                for (int i = 0; i < K; i++)
+                {
+                    if (result[i] == null)
+                        result[i] = new List<int>();
+
+                    if (number >= Intervals[i] && number <= Intervals[i + 1])
+                        result[i].Add(number);
+                }
+            }
+
+            return result;
+        }
+
+        private double[] CalculateF()
+        {
+            var dataByIntervals = DistributeDataByIntervals();
+
+            var result = new double[K];
+
+            for (int i = 0; i < K; i++)
+                result[i] = dataByIntervals[i].Count / (N * IntervalWidth);
+
+            return result;
+        }
+
+        private List<double> CalculateP()
+        {
+            var fValues = CalculateF();
+            var result = new List<double>();
+
+            for (int i = 0; i < K; i++)
+            {
+                double temp = 0f;
+                for (var j = 0; j < i + 1; j++)
+                    temp += fValues[j] * IntervalWidth;
+
+                result.Add(1 - temp);
+            }
+
+            return result;
+        }
+
+        private int GetInterval(double number)
+        {
+            for (int i = 0; i < K; i++)
+            {
+                if (number >= Intervals[i] && number <= Intervals[i+1])
+                    return i;
+            }
+            return -1;
+        }
+
+        public double GetTValue(double percentage)
+        {
+            var pValues = CalculateP();
+            pValues.Insert(0, 1);
+
+            for (int i = 0; i < pValues.Count; i++)
+            {
+                if (pValues[i] > percentage)
+                {
+                    pValues.Insert(i + 1, percentage);
+                    break;
+                }
+            }
+
+            var index = pValues.IndexOf(percentage);
+            var d = (pValues[index + 1] - percentage) / (pValues[index + 1] - pValues[index - 1]);
+            return Intervals[index] - IntervalWidth * d;
+        }
+
+        public double GetProbabilityOfWorkWithoutBreakdown(int hoursCount)
+        {
+            var fValues = CalculateF();
+            var intervalNumber = GetInterval(hoursCount);
+            double temp = 0f;
+
+            for (int i = 0; i < intervalNumber + 1; i++)
+            {
+                if (i != intervalNumber)
+                    temp += fValues[i] * IntervalWidth;
+                else
+                    temp += fValues[i] * (hoursCount - Intervals[i]);
+            }
+
+            return 1 - temp;
+        }
+
+        public double GetIntensityOfBreakdowns(int hoursCount)
+        {
+            var fValues = CalculateF();
+            var intervalNumber = GetInterval(hoursCount);
+            var probability = GetProbabilityOfWorkWithoutBreakdown(hoursCount);
+
+            return fValues[intervalNumber] / probability;
+        }
+    }
+}
---
+++ ReliabilityOfCompSystems/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ReliabilityOfCompSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[] 
+            { 
+                166, 1975, 995, 1897, 1253, 840, 181, 1024, 
+                383, 967, 260, 188, 1420, 53, 2893, 67, 787, 
+                784, 667, 1572, 1480, 589, 607, 270, 618,
+                396, 518, 1307, 204, 160, 1461, 1850, 952,
+                5767, 3518, 56, 829, 168, 664, 695, 2237, 
+                298, 1154, 542, 403, 1541, 76, 950, 847, 
+                1054, 72, 1296, 886, 486, 26, 2098, 463, 
+                1815, 1416, 1743, 218, 344, 1269, 288,
+                1137, 175, 39, 293, 618, 794, 2142, 40, 915,
+                676, 1640, 166, 538, 875, 215, 211, 2959,
+                961, 140, 28, 649, 457, 1935, 727, 1545, 76, 
+                46, 2007, 1881, 689, 398, 340, 8, 2959, 461,
+                328
+            };
+
+            double percentage = 0.63f; 
+            int hoursCountForProbability = 3168; // hours without breakdown
+            int hoursCountForIntencity = 5210;
+            int k = 10;
+
+            Lab1 lab1 = new Lab1(k, values);
+
+            var tValue = lab1.GetTValue(percentage);
+            var probability = lab1.GetProbabilityOfWorkWithoutBreakdown(hoursCountForProbability);
+            var intensity = lab1.GetIntensityOfBreakdowns(hoursCountForIntencity);
+
+            Console.WriteLine($"Mean value of time for breakdown: {tValue:0.##}");
+            Console.WriteLine($"Probability of work without breakdown for {hoursCountForProbability} hours: {probability:0.####}");
+            Console.WriteLine($"Intensity of breakdowns for {hoursCountForIntencity} hours of work: {intensity:0.####}");
+        }
+    }
+}
